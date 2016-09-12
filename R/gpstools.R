d32ddec298d77beeb2d39eaa880a81ee9f68cd34
rpatin <remi.patin@free.fr>
2016-09-12 06:31:30 +0000

    Add gpstools.R, for package description in the manual

---
+++ R/gpstools.R
@@ -0,0 +1,10 @@
+#' gpstools: tools for analysis of animal GPS movement data
+#'
+#' gpstools provides usefull tools for the analysis of animal movement such as
+#' resampling trajectories with irregular data, creating timelines for
+#' summarizing data, ...
+#'
+#' @docType package
+#' @name gpstools
+
+NULL
